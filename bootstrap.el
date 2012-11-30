4356c37fc6c6c0f881de709f55bedc10ffa93585
Vitor Capela <dodecaphonic@gmail.com>
2012-11-30 20:44:13 -0200

    Add rinari back to the mix.

--- bootstrap.el
+++ bootstrap.el
@@ -5,7 +5,7 @@
   '(clojure-mode coffee-mode expand-region gist inf-ruby ruby-mode
     magit markdown-mode solarized-theme yaml-mode rspec-mode csharp-mode
     rainbow-mode json scala-mode rvm slime yasnippet-bundle
-    color-theme-sanityinc-tomorrow)
+    color-theme-sanityinc-tomorrow rinari)
   "A list of packages to ensure are installed at launch.")
 
 (defun bootstrap-required-packages-installed-p ()
