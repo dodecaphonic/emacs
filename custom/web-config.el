7f7c21231d40a1528ce994cadb91d0f7a850386d
Vitor Capela <dodecaphonic@gmail.com>
2013-02-04 16:07:39 -0200

    Adds long-due mapping between erb and less files to html and css modes.

--- custom/web-config.el
+++ custom/web-config.el
@@ -1,3 +1,5 @@
 (require 'smarttabs)
 
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . html-mode))
