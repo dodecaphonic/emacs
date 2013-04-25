44142dc5134aaa2275651244817fce8c2c6fd0bb
Vitor Capela <dodecaphonic@gmail.com>
2013-04-25 17:01:41 -0300

    Moves lines up and down.

---
+++ utilities/move-lines.el
@@ -0,0 +1,15 @@
+;; from http://emacsredux.com/blog/2013/04/02/move-current-line-up-or-down/
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
