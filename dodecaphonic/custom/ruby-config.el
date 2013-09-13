1a318a6d738fbcd9b151e82da0142b3afa764975
Vitor Capela <dodecaphonic@gmail.com>
2013-09-13 17:51:05 -0300

    Add xmpfilter magic to the mix.

--- dodecaphonic/custom/ruby-config.el
+++ dodecaphonic/custom/ruby-config.el
@@ -9,7 +9,8 @@
 			   (delete-trailing-whitespace))))
 	    (set (make-local-variable 'indent-tabs-mode) 'nil)
 	    (set (make-local-variable 'tab-width) 2)
-            (local-set-key (kbd "C-c =") 'dodecaphonic/align=)))
+            (local-set-key (kbd "C-c =") 'dodecaphonic/align=)
+            (local-set-key (kbd "C-c C-c") 'xmp)))
 
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
 (custom-set-variables 
