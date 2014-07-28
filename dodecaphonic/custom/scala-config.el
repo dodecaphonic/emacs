56ac1bf2aac154b5decd6811a3e9000b4a6fd6ec
Vitor Capela <dodecaphonic@gmail.com>
2014-07-28 17:03:47 -0300

    Fix loading of ensime-related crap.

--- dodecaphonic/custom/scala-config.el
+++ dodecaphonic/custom/scala-config.el
@@ -1,4 +1,7 @@
 ;; Stolen from bodil/emacs.d/bodil-haskell.el
+(require 'scala-mode2)
+(require 'ensime)
+
 (font-lock-replace-symbol 'scala-mode "\\(->\\)" "→")
 (font-lock-replace-symbol 'scala-mode "\\(<-\\)" "←")
 (font-lock-replace-symbol 'scala-mode "\\(=>\\)" "⇒")
@@ -9,6 +12,4 @@
 (define-key scala-mode-map (kbd "⇒") (lambda () (interactive) (insert "=>")))
 (define-key scala-mode-map (kbd "⇐") (lambda () (interactive) (insert "<=")))
 
-(add-to-list 'load-path (concat emacs-root "vendor/ensime/elisp"))
-(require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
