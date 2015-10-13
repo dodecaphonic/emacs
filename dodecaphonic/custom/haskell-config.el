b5f99550713a23a12c713fb97bfa94cd42e6e52b
Vitor Capela <dodecaphonic@gmail.com>
2015-10-12 21:16:40 -0300

    Add more haskell config

--- dodecaphonic/custom/haskell-config.el
+++ dodecaphonic/custom/haskell-config.el
@@ -1,4 +1,22 @@
 (defun dodecaphonic-haskell-mode-hook ()
-  (haskell-indentation-mode))
+  (haskell-indentation-mode t)
+  (local-set-key (kbd "C-,") 'haskell-move-nested-left)
+  (local-set-key (kbd "C-.") 'haskell-move-nested-right)
+  (local-set-key [f8] 'haskell-navigate-imports)
+  (local-set-key (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (local-set-key (kbd "C-`") 'haskell-interactive-bring)
+  (local-set-key (kbd "C-c C-t") 'haskell-process-do-type)
+  (local-set-key (kbd "C-c C-i") 'haskell-process-do-info)
+  (local-set-key (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (local-set-key (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (local-set-key (kbd "C-c c") 'haskell-process-cabal)
+  (local-set-key (kbd "SPC") 'haskell-mode-contextual-space))
 
 (add-hook 'haskell-mode-hook 'dodecaphonic-haskell-mode-hook)
+
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(add-hook 'haskell-mode-hook #'hindent-mode)
