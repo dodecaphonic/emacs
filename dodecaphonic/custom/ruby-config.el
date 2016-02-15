4f10d1f33e20070b89699cdd27ab6e23ad5bc28e
Vitor Capela <dodecaphonic@gmail.com>
2016-02-15 21:50:29 -0200

    Remove enh-ruby-mode

--- dodecaphonic/custom/ruby-config.el
+++ dodecaphonic/custom/ruby-config.el
@@ -3,22 +3,20 @@
 (defun dodecaphonic-ruby-mode-hook ()
   (set (make-local-variable 'indent-tabs-mode) 'nil)
   (set (make-local-variable 'tab-width) 2)
-  (setq enh-ruby-bounce-deep-indent t)
-  (setq enh-ruby-hanging-brace-indent-level 2)
   (local-set-key (kbd "C-c =") 'dodecaphonic/align=)
   (local-set-key (kbd "C-c C-c") 'xmp)
   (local-set-key (kbd "C-c C-d") 'duplicate-current-line))
 
-(add-hook 'enh-ruby-mode-hook 'dodecaphonic-ruby-mode-hook)
+(add-hook 'ruby-mode-hook 'dodecaphonic-ruby-mode-hook)
 
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
-(add-hook 'enh-ruby-mode-hook 'ruby-tools-mode)
-(add-hook 'enh-ruby-mode-hook 'ruby-refactor-mode-launch)
-(add-hook 'enh-ruby-mode-hook 'rubocop-mode)
-(add-hook 'enh-ruby-mode-hook 'flycheck-mode)
-(add-hook 'enh-ruby-mode-hook 'yard-mode)
+(add-hook 'ruby-mode-hook 'ruby-tools-mode)
+(add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
+(add-hook 'ruby-mode-hook 'rubocop-mode)
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+(add-hook 'ruby-mode-hook 'yard-mode)
 
 (custom-set-variables
  '(ruby-refactor-add-parens t)
