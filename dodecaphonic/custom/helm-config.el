f8740627f5b5100b604b5a1eb06014bbcd843a4e
Vitor Capela <dodecaphonic@gmail.com>
2016-04-21 17:13:02 -0300

    Switch helm-buffers-list keybinding

--- dodecaphonic/custom/helm-config.el
+++ dodecaphonic/custom/helm-config.el
@@ -5,7 +5,7 @@
 (helm-projectile-on)
 
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-c p f") 'helm-projectile-find-file)
 (global-set-key (kbd "C-c p a") 'helm-projectile-find-other-file)
