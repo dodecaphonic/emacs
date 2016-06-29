80d8d6c09cfe1cff572acf23592526c0a8c3d16b
Vitor Capela <dodecaphonic@gmail.com>
2016-06-29 07:52:14 -0300

    Add psc-ide

--- dodecaphonic/custom/purescript-config.el
+++ dodecaphonic/custom/purescript-config.el
@@ -2,6 +2,15 @@
   :commands purescript-mode
   :mode (("\\.purs$" . purescript-mode))
   :config
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation))
+  (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
+
+  (use-package psc-ide
+    :config
+    (defun dodecaphonic-purescript-mode-hook ()
+      (psc-ide-mode t)
+      (company-mode t)
+      (flycheck-mode t))
+
+    (add-hook 'purescript-mode-hook 'dodecaphonic-purescript-mode-hook)))
 
 (provide 'purescript-config)
