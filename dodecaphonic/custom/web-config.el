e90f0c265aa6d4e51eb8f84bc815196a6a6978e5
Vitor Capela <dodecaphonic@gmail.com>
2016-07-29 15:12:20 -0300

    Add jsx as a web-mode show

--- dodecaphonic/custom/web-config.el
+++ dodecaphonic/custom/web-config.el
@@ -6,7 +6,8 @@
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-code-indent-offset 2)
+  (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode)))
 
 (use-package rainbow-mode)
 
