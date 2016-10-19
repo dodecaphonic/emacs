78fa681e5adeffbf4e2ae63ad008bc57fe8a4bac
Vitor Capela <dodecaphonic@gmail.com>
2016-10-19 15:35:09 -0200

    Add .tsx config to typescript

--- dodecaphonic/custom/typescript-config.el
+++ dodecaphonic/custom/typescript-config.el
@@ -10,3 +10,14 @@
       (company-mode t))
 
     (add-hook 'typescript-mode-hook #'dodecaphonic-tide-setup)))
+
+(with-eval-after-load "web-config"
+  (require 'web-mode)
+
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (tide-setup)))))
+
+(provide 'typescript-config)
