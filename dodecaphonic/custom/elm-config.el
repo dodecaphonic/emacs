d3b6743ec86500bba7ef29811cd41a059544c0d1
Vitor Capela <dodecaphonic@gmail.com>
2016-10-19 15:35:49 -0200

    Use company completion with Elm

--- dodecaphonic/custom/elm-config.el
+++ dodecaphonic/custom/elm-config.el
@@ -4,6 +4,12 @@
   (add-to-list 'exec-path elm-format-path)
   (setenv "PATH" (concat (getenv "PATH") (concat ":" elm-format-path)))
   :config
-  (setq elm-format-on-save t))
+  (defun dodecaphonic-elm-mode-hook ()
+    (company-mode t)
+    (add-to-list 'company-backends 'company-elm))
+
+  (setq elm-format-on-save t)
+  (add-hook 'elm-mode-hook 'dodecaphonic-elm-mode-hook)
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-completion))
 
 (provide 'elm-config)
