2df681cc9c37bac7b806a5b38a721cf43105a176
Vitor Capela <dodecaphonic@gmail.com>
2018-07-11 13:48:30 -0300

    Show gravatars in magit revision buffers

--- dodecaphonic/custom/magit-config.el
+++ dodecaphonic/custom/magit-config.el
@@ -1,5 +1,7 @@
 (use-package magit
   :commands magit-status
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
+  :custom
+  (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")))
 
 (provide 'magit-config)
