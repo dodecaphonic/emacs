5a97b598831af7a42ff8112f513b0d51eadaf3d8
Vitor Capela <dodecaphonic@gmail.com>
2020-09-24 12:12:58 -0300

    Remove org-roam

--- dodecaphonic/custom/writing-config.el
+++ dodecaphonic/custom/writing-config.el
@@ -6,20 +6,6 @@
 
 (use-package org)
 
-(use-package org-roam
-      :ensure t
-      :hook
-      (after-init . org-roam-mode)
-      :custom
-      (org-roam-directory "/home/dodecaphonic/Dropbox/Notes")
-      :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph-show))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate))))
-
 (use-package darkroom)
 
 (provide 'writing-config)
