eb84d816454b15ee244af622eadfb5d191f6d6cc
Vitor Capela <vitor@braunecapela.com>
2026-07-26 17:52:48 -0300

    Adjust what <ret> does in magit-mode

--- dodecaphonic/custom/magit-config.el
+++ dodecaphonic/custom/magit-config.el
@@ -1,5 +1,7 @@
 (use-package magit :ensure t
   :commands magit-status
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
+  :config
+  (define-key magit-mode-map (kbd "RET") 'magit-diff-visit-worktree-file))
 
 (provide 'magit-config)
