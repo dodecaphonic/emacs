377ecabf99654ddfab3079fd5be2aed7594cb312
Vitor Capela <vitor@braunecapela.com>
2026-07-26 17:19:44 -0300

    Adjust projectile to pick up on new files

--- dodecaphonic/custom/projectile-config.el
+++ dodecaphonic/custom/projectile-config.el
@@ -3,6 +3,9 @@
   :diminish projectile-mode
   :config
   (setq projectile-enable-caching t)
+  (setq projectile-auto-update-cache t)
+  (setq projectile-auto-update-cache-with-watches t)
+  (setq projectile-verbose t)
   (add-hook 'after-init-hook #'projectile-global-mode))
 
 (provide 'projectile-config)
